59343be1f652704153281064cf5c80e76fa302df
Shashiverm <meetshashikantkumar@gmail.com>
2026-09-25 00:01:15 +0530

    chore: add environment template, dedicated vitest configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // @ts-expect-error vitest config
-  test: {
-    include: ['tests/**/*.test.ts'],
-    exclude: ['tests/e2e/**', 'node_modules/**', 'dist/**'],
-  },
 })
